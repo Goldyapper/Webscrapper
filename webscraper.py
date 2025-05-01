7902db3b9bd31f77425a1a55e229dd23b7ef9c6d
Adam Parsons <ParsonsA10@cardiff.ac.uk>
2025-05-01 10:49:18 +0100

    doctor retrival works

--- webscraper.py
+++ webscraper.py
@@ -1,8 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
 def fetch_data(url):
     # URL of the website to fetch data from
     url = "https://tardis.wiki/wiki/The_Chimes_of_Midnight_(audio_story)"
+
 
     try:
         # Send a GET request to the website
@@ -10,9 +12,17 @@ def fetch_data(url):
         response.raise_for_status()  # Raise an error for bad status codes
 
         # Access the content of the response
-        data = response.text
         print("Data fetched successfully:")
-        return(data)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        for item in soup.select('div.pi-item'): # a for loop that runs through at elements in the table
+            label = item.select_one('h3.pi-data-label')
+            #Doctor Data retrival
+            if label and 'Doctor:' in label.text:
+                values = item.select('div.pi-data-value a')
+                doctor = [d.text for d in values]
+
+        return(doctor)
 
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
